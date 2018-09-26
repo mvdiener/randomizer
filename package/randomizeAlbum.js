0fc4fa275af3b7631f3b2e0c8f2b7d9c73846e30
Max Diener <max.diener@callruby.com>
2018-09-26 13:46:36 -0700

    Fixed indentation, added patch notes to readme, bumped version

--- package/randomizeAlbum.js
+++ package/randomizeAlbum.js
@@ -1,7 +1,7 @@
 if (window.location.href == "https://open.spotify.com/collection/albums") {
-	var albumArray = document.querySelectorAll('.cover-art-playback');
-	if (albumArray.length > 0) {
-		var albumIndex = Math.floor(Math.random() * (albumArray.length))
-		albumArray[albumIndex].click();
-	}
+    var albumArray = document.querySelectorAll('.cover-art-playback');
+    if (albumArray.length > 0) {
+        var albumIndex = Math.floor(Math.random() * (albumArray.length))
+        albumArray[albumIndex].click();
+    }
 }
