fb2b01098cba11c856f93cffb2a1207f6e10980c
Max Diener <max.diener@callruby.com>
2018-11-06 15:17:31 -0800

    Updated tags due to changes on spotify's front end

--- package/orderAlbums.js
+++ package/orderAlbums.js
@@ -2,8 +2,8 @@ if (window.location.href == "https://open.spotify.com/collection/albums") {
     var albumElement = document.querySelectorAll('.container-fluid--noSpaceAround')[0].children[0];
     var albumArray = Array.from(albumElement.children);
     var sortedArray = albumArray.sort(function (a, b) {
-        aArtist = a.querySelector('.ellipsis-one-line').innerHTML == "Various artists" ? a.querySelector('.ellipsis-one-line').innerHTML.toLowerCase() : a.querySelector('.ellipsis-one-line').getElementsByTagName('a')[0].innerText.toLowerCase();
-        bArtist = b.querySelector('.ellipsis-one-line').innerHTML == "Various artists" ? b.querySelector('.ellipsis-one-line').innerHTML.toLowerCase() : b.querySelector('.ellipsis-one-line').getElementsByTagName('a')[0].innerText.toLowerCase();
+        aArtist = a.querySelector('.ellipsis-one-line span') == null ? a.querySelector('.ellipsis-one-line a').innerHTML.toLowerCase() : a.querySelector('.ellipsis-one-line span').innerText.toLowerCase();
+        bArtist = b.querySelector('.ellipsis-one-line span') == null ? b.querySelector('.ellipsis-one-line a').innerHTML.toLowerCase() : b.querySelector('.ellipsis-one-line span').innerText.toLowerCase();
 
         if(aArtist < bArtist) return -1;
         if(aArtist > bArtist) return 1;
